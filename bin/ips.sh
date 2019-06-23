82df12e2aca804d13a7aed20a97b3c700bf4c4bc
sonofborge <sonofborge@gmail.com>
2019-06-23 16:09:50 -0700

    cleanup

---
+++ bin/ips.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+printf "\n"
+echo "nextcloud-app:"
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nextcloud-app
+printf "\n"
+echo "nextcloud-letsencrypt:"
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nextcloud-letsencrypt
+printf "\n"
+echo "nextcloud-proxy:"
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nextcloud-proxy
+printf "\n"
+echo "nextcloud-mariadb:"
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nextcloud-mariadb
+printf "\n"
