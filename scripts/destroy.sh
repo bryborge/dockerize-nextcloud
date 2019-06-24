56a0fa5cbe0c6215caf125a182c81646bc780418
sonofborge <sonofborge@gmail.com>
2019-06-23 22:00:27 -0700

    write start script

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -15,7 +15,7 @@ docker volume rm dockerizenextcloud_nextcloud
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && cd .. && pwd )"
 
 printf "\n"
-echo "WARNING: YOU'RE ABOUT TO DELETE ALL YO' SHIT !!!"
+echo "WARNING: YOU'RE ABOUT TO DELETE ALL YOUR DATA!!!"
 echo "================================================"
 printf "\n"
 echo "Do you want to DELETE local 'app' and 'proxy' data?"
@@ -24,6 +24,7 @@ read -p "(Enter y or n): " confirm
 if [[ "y" == $confirm ]]; then
   echo "Deleting local data ..."
   sudo rm -rf $PROJECT_DIR/app && sudo rm -rf $PROJECT_DIR/proxy
+  rm $PROJECT_DIR/.env
   echo "Complete."
 else
   echo "Skipping deletion of local data."
--- scripts/list-ips.sh
+++ scripts/list-ips.sh
@@ -6,9 +6,6 @@ printf "\n"
 echo "nextcloud-app:"
 docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nextcloud-app
 printf "\n"
-echo "nextcloud-letsencrypt:"
-docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nextcloud-letsencrypt
-printf "\n"
 echo "nextcloud-proxy:"
 docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nextcloud-proxy
 printf "\n"
